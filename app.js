da1c89ac59a876896e8bd3509d981f2f973b798c
Amir Mustafa <amirengg15@gmail.com>
2024-03-10 20:20:50 +0530

    feat: adding events, createEvents resolvers, grapgqlHTTP fix

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const graphqlHttp = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/graphql', graphqlHttp({
+app.use('/graphql', graphqlHTTP({
     schema: buildSchema(`
         type RootQuery {
             events: [String!]!
@@ -22,7 +22,16 @@ app.use('/graphql', graphqlHttp({
             mutation: RootMutation
         }
     `),
-    rootValue: {}
+    rootValue: {
+        events: () => {
+            return ['Romantic Cooking', 'Watching Suits', 'Coding'];
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
 }));
 
 app.listen(3000);
