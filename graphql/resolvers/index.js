c3556a0f1f1ea53a8665f443f6aac7205b0d0dc7
Amir Mustafa <amirengg15@gmail.com>
2024-03-22 06:13:04 +0530

    refactor: dateToString reusable method

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -2,11 +2,12 @@ const bcrypt = require('bcryptjs');
 const Event = require('../../models/event');
 const User = require('../../models/user');
 const Booking = require('../../models/booking');
+const { dateToString } = require('../../helpers/date');
 
 const transformEvent = event => {
     return {
         ...event._doc,
-        date: new Date(event._doc.date).toISOString(),
+        date: dateToString(event._doc.date),
         creator: user.bind(this, event._doc.creator)
     };
 }
@@ -65,8 +66,8 @@ module.exports = {
                     ...booking._doc,
                     user: user.bind(this, booking._doc.user),
                     event: singleEvent.bind(this, booking._doc.event),
-                    createdAt: new Date(booking._doc.createdAt).toISOString(),
-                    updatedAt: new Date(booking._doc.updatedAt).toISOString()
+                    createdAt: dateToString(booking._doc.createdAt),
+                    updatedAt: dateToString(booking._doc.updatedAt)
                 }
             });
         } catch (err) {
@@ -129,8 +130,8 @@ module.exports = {
                 ...result._doc,
                 user: user.bind(this, booking._doc.user),
                 event: singleEvent.bind(this, booking._doc.event),
-                createdAt: new Date(result._doc.createdAt).toISOString(),
-                updatedAt: new Date(result._doc.updatedAt).toISOString()
+                createdAt: dateToString(result._doc.createdAt),
+                updatedAt: dateToString(result._doc.updatedAt)
             }
         } catch (err) {
             throw err;
