0d964f96483eb976144fbe63e64a66c8ec0d78fd
Amir Mustafa <amirengg15@gmail.com>
2024-03-27 06:16:30 +0530

    feat: authenticate login form

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import './Auth.css';
 
 const Auth = () => {
     return (
         <div>
-            <h2>The Auth Page</h2>
+            <form className='auth-form'>
+                <div className='form-control'>
+                    <label htmlFor='email'>Email: </label>
+                    <input type="email" id="email" />
+                </div>
+                <div className='form-control'>
+                    <label htmlFor='password'>Password: </label>
+                    <input type="password" id="password" />
+                </div>
+                <div className='form-actions'>
+                    <button type="submit">Submit</button>
+                    <button type="button">Switch to Signup</button>
+                </div>
+            </form>
         </div>
     );
 }
