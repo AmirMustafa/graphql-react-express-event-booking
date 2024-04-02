39dbef196b9fba496dec0551834f7afa00c06670
Amir Mustafa <amirengg15@gmail.com>
2024-04-02 23:47:32 +0530

    feat: opening modal on create event button click

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,18 +1,30 @@
-import React, { Fragment } from 'react';
+import React, { useState, Fragment } from 'react';
 import Modal from '../components/Navigation/Modal/Modal';
 import Backdrop from '../components/Backdrop/Backdrop';
 import './Events.css';
 
 const Events = () => {
+    const [creating, setCreating] = useState(false);
+
+    const startCreateHandler = () => {
+        setCreating(true);
+    }
+
+    const modalCancelHandler = () => {
+        setCreating(false);
+    }
+    const modalConfirmHandler = () => {
+        setCreating(false);
+    }
     return (
         <Fragment>
-            <Backdrop />
-            <Modal title="Create Modal" canCancel canConfirm>
+            {creating && <Backdrop />}
+            {creating && <Modal title="Create Modal" canCancel canConfirm onCancel={modalCancelHandler} onConfirm={modalConfirmHandler}>
                 <p>Modal Content</p>
-            </Modal>
+            </Modal>}
             <div className='events-control'>
                 <p>Create your own event!</p>
-                <button className='btn'>Create Event</button>
+                <button className='btn' onClick={startCreateHandler}>Create Event</button>
             </div>
         </Fragment>
     );
