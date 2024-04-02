59ca4001a07ba2077638a80c4f4cfde650b503a3
Amir Mustafa <amirengg15@gmail.com>
2024-04-03 05:01:26 +0530

    feat: events html form on Modal

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -20,7 +20,24 @@ const Events = () => {
         <Fragment>
             {creating && <Backdrop />}
             {creating && <Modal title="Create Modal" canCancel canConfirm onCancel={modalCancelHandler} onConfirm={modalConfirmHandler}>
-                <p>Modal Content</p>
+                <form>
+                    <div className='form-control'>
+                        <label htmlFor='title'>Title</label>
+                        <input type="text" id="title" />
+                    </div>
+                    <div className='form-control'>
+                        <label htmlFor='price'>Price</label>
+                        <input type="number" id="price" />
+                    </div>
+                    <div className='form-control'>
+                        <label htmlFor='date'>Date</label>
+                        <input type="date" id="date" />
+                    </div>
+                    <div className='form-control'>
+                        <label htmlFor='description'>Description</label>
+                        <textarea id='description' rows="4"></textarea>
+                    </div>
+                </form>
             </Modal>}
             <div className='events-control'>
                 <p>Create your own event!</p>
